3594479040197268f08a9d4f16fe33107fbed7a9
jpt4-crux <torreaiworks01@gmail.com>
2018-11-27 04:27:56 +0000

    prime-index using eratosthenes.

--- xeno.scm
+++ xeno.scm
@@ -10,6 +10,25 @@
 	 (string->symbol (list->string (list a)))) 
        (string->list (symbol->string tic-symbol))))
 
+;sieve of eratosthenes
+;primes [2, n]
+(define (eratosthenes n)
+  (let ([num-vec (list->vector (iota (+ 1 n)))])
+    (let inc-loop ([inc 2])
+      (if (> inc (floor (sqrt n)))
+	  ;return
+	  (let ([primes '()])
+	    (vector-for-each (lambda (a) (if (not (equal? 'c a))
+					     (set! primes (cons a primes))))
+			     num-vec)
+	    (cddr (reverse primes)))			   
+	  (let ind-loop ([ind (+ inc inc)])
+	    (cond
+	     [(> ind n) (inc-loop (+ 1 inc))]
+	     [(vector-set! num-vec ind 'c) (ind-loop (+ ind inc))])
+	    )))
+    ))
+
 (define primes '(2 3 5 7 11 13 17 19 23 29 31 37 41))
 
 (define new-primes 
@@ -30,6 +49,21 @@
 	 (expt (list-ref new-primes (- (length new-primes) 1)) 2)))
       (list-ref new-primes (- i 1))))
 
+(define primes-list (eratosthenes 1000))
+
+;last element of a list
+(define (last-element ls) (car (last-pair ls)))
+
+(define (square n) (expt n 2))
+
+(define (prime-index1 i)
+  (if (> i (length primes-list))
+      (begin
+      (set! primes-list
+	(eratosthenes (square (last-element primes-list))))
+      (list-ref primes-list (- i 1)))
+      (list-ref primes-list (- i 1))))
+
 ; ::(:) -> '(: : (:))
 (define (x->a x)
   (cond
@@ -37,6 +71,14 @@
    [(equal? (car x) ':) (* 2 (x->a (cdr x)))]
    [(null? (cdr x)) (prime-index (x->a (car x)))]
    [(not (null? (cdr x))) (* (x->a (list (car x))) (x->a (cdr x)))]
+   ))
+
+(define (x->a1 x)
+  (cond
+   [(null? x) 1]
+   [(equal? (car x) ':) (* 2 (x->a1 (cdr x)))]
+   [(null? (cdr x)) (prime-index1 (x->a1 (car x)))]
+   [(not (null? (cdr x))) (* (x->a1 (list (car x))) (x->a1 (cdr x)))]
    ))
 
 ;integers from n to m inclusive
@@ -71,24 +113,7 @@
 	    ))])
      (loop primels sieve-maximum))))
   
-;sieve of eratosthenes
-;primes [2, n]
-(define (eratosthenes n)
-  (let ([num-vec (list->vector (iota (+ 1 n)))])
-    (let inc-loop ([inc 2])
-      (if (> inc (floor (sqrt n)))
-	  ;return
-	  (let ([primes '()])
-	    (vector-for-each (lambda (a) (if (not (equal? 'c a))
-					     (set! primes (cons a primes))))
-			     num-vec)
-	    (cddr (reverse primes)))			   
-	  (let ind-loop ([ind (+ inc inc)])
-	    (cond
-	     [(> ind n) (inc-loop (+ 1 inc))]
-	     [(vector-set! num-vec ind 'c) (ind-loop (+ ind inc))])
-	    )))
-    ))
+
 
 (define (eratosthenes-old n)
   (let ([num-vec (list->vector (iota (+ 1 n)))])
