75803241102b5562b5e1ce62ee030d7205f50007
jpt4 <jpt4@protonmail.ch>
2024-01-02 23:58:08 +0000

    Begin xeno in core.logic.

---
+++ xeno/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to xeno
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ xeno/src/xeno/core.clj
@@ -0,0 +1,31 @@
+;;xeno/src/core.clj
+;;20240102Z
+;;jpt4
+;;xenotation transliteration in core.logic
+
+(ns xeno.core
+  (:require [clojure.core.logic :as lgc
+             :refer :all :rename {== ==o}]
+            [clojure.core.logic.fd :as fd]
+            :reload-all))
+
+;;iota - list of natural numbers from 0 to n-1
+(defn ioto [n ns]
+
+  
+;;Sieve of Erathosthenes - produce primes from 2 to n.
+(defn soeo [n ps]
+  ""
+)
+   
+  
+
+(defn xeno [a x n]
+  ""
+  )
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
